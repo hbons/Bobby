6c8ecc396845dc5fc86ee8c502c75b8a9f181a93
Hylke Bons <hello@planetpeanut.studio>
2026-01-09 11:29:25 +0100

    Bump libadwaita to 1.8

--- src/gtk/about.rs
+++ src/gtk/about.rs
@@ -5,24 +5,23 @@
 //   under the terms of the GNU General Public License v3 or any later version.
 
 
-use gtk4::prelude::*;
 use gtk4::{ License, Window, };
-use libadwaita::AboutWindow;
+use libadwaita::AboutDialog;
+use libadwaita::prelude::AdwDialogExt;
 
 
 pub fn show_about_dialog(parent: &Window) {
-    let about = AboutWindow::builder() // TODO: from_appdata() if running in Flatpak?
-        .transient_for(parent)
+    let about = AboutDialog::builder() // TODO: from_appdata() if running in Flatpak?
         .application_name(env!["CARGO_PKG_NAME"])
         .application_icon("studio.planetpeanut.Bobby")
         .developer_name("Hylke Bons")
         .version(env!["CARGO_PKG_VERSION"])
-        // .developers(["Hylke Bons"])
-        // .designers(["Hylke Bons"])
+        .debug_info(
+            format!("SQLite {}", rusqlite::version()))
         .license_type(License::Gpl30)
         .website(env!["CARGO_PKG_HOMEPAGE"])
         .issue_url(env!["CARGO_PKG_REPOSITORY"])
         .build();
 
-    about.present();
+    about.present(Some(parent));
 }
